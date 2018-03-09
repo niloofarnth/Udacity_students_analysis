ad76c51ac2b1a4895f64772a832860622eae597b
Niloofar Tehrani <niloofartehrani@Niloofars-MacBook-Air.local>
2018-03-08 23:38:20 -0600

    change function name from file_opener to file_reader

--- data-analysis.py
+++ data-analysis.py
@@ -6,12 +6,13 @@ This is a temporary script file.
 """
 
 import unicodecsv
-def file_opener(file_name):
+def file_reader(file_name):
     with open (path + file_name , "rb") as file:
         reader = unicodecsv.DictReader(file)
         return list(reader)
     
 path = "/Users/niloofartehrani/google drive/programming/data-analysis/"
-daily_engagement = file_opener("daily_engagement.csv")
-project_submissions = file_opener("project_submissions.csv")
+daily_engagement = file_reader("daily_engagement.csv")
+project_submissions = file_reader("project_submissions.csv")
 
+print (daily_engagement[0])
