052506d02afe197d6ae1f9a06fe5767d85e00d8d
Niloofar Tehrani <niloofartehrani@Niloofars-MacBook-Air.local>
2018-03-09 11:42:50 -0600

    find the odd students among who are not in the engagement data

--- data-analysis.py
+++ data-analysis.py
@@ -81,3 +81,11 @@ for student in enrollment_unique_students:
         continue
 #print (not_in_engagement)
 
+    
+problem_students = []
+for student in not_in_engagement:
+    for row in enrollments:
+        if student == row["account_key"]:
+            if row["days_to_cancel"] != 0:
+                problem_students.append(row)
+print (problem_students)
